656fd0748247579903acd55fc770e589efd21e11
Adelice <nancytoniw@gmail.com>
2019-05-08 20:44:59 +0200

    deploy

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nancy/Desktop/Add-to-Cart/virtual"
+set -gx VIRTUAL_ENV "/home/nancy/Desktop/Cart/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
